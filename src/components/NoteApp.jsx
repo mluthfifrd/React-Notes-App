6af28d3ea7a68003a715f7972664d5a0513468ae
M.Luthfi Farid <82927435+mluthfifrd@users.noreply.github.com>
2023-11-30 07:25:42 +0700

    add login and register page

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -1,22 +1,109 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Route, Routes, Navigate, useNavigate } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import ArchivePage from '../pages/ArchivePage'
 import AddNotePage from '../pages/AddNotePage'
 import DetailPageWrapper from '../pages/DetailPage'
 import NotFoundPage from '../pages/NotFoundPage'
+import LoginPage from '../pages/auth/LoginPage'
+import RegisterPage from '../pages/auth/RegisterPage'
+import Header from '../pages/layout/Header'
+import { getUserLogged, putAccessToken } from '../utils/network-data'
 
 function NoteApp() {
+  const navigate = useNavigate()
+  const [authedUser, setAuthedUser] = useState(null)
+  const [initializing, setInitializing] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await getUserLogged()
+        setAuthedUser(data)
+        setInitializing(false)
+      } catch (error) {
+        // Handle errors if necessary
+        console.error('Error fetching user data:', error)
+        setInitializing(false)
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  const onLoginSuccess = async ({ accessToken }) => {
+    putAccessToken(accessToken)
+    const { data } = await getUserLogged()
+    setAuthedUser(data)
+    navigate('/')
+  }
+
+  const onLogout = () => {
+    setAuthedUser(null)
+    putAccessToken('')
+    navigate('/login')
+  }
+
+  if (initializing) {
+    return (
+      <div className="load-login">
+        <h1>AMBIL DATA</h1>
+      </div>
+    )
+  }
+
+  if (authedUser === null) {
+    return (
+      <div className="app-container">
+        <Header authUser={authedUser} />
+        <main>
+          <Routes>
+            <Route path="/" element={authedUser ? <HomePage /> : <Navigate to="/login" />} />
+            <Route
+              path="/archives"
+              element={authedUser ? <ArchivePage /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/notes/new"
+              element={authedUser ? <AddNotePage /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/notes/:id"
+              element={authedUser ? <DetailPageWrapper /> : <Navigate to="/login" />}
+            />
+            <Route path="/login" element={<LoginPage loginSuccess={onLoginSuccess} />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+      </div>
+    )
+  }
+
   return (
-    <main>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/archives" element={<ArchivePage />} />
-        <Route path="/notes/new" element={<AddNotePage />} />
-        <Route path="/notes/:id" element={<DetailPageWrapper />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </main>
+    <div className="app-container">
+      <Header logout={onLogout} name={authedUser.name} authUser={authedUser} />
+      <main>
+        <Routes>
+          <Route path="/" element={authedUser ? <HomePage /> : <Navigate to="/login" />} />
+          <Route
+            path="/archives"
+            element={authedUser ? <ArchivePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/notes/new"
+            element={authedUser ? <AddNotePage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/notes/:id"
+            element={authedUser ? <DetailPageWrapper /> : <Navigate to="/login" />}
+          />
+          <Route path="/login" element={<LoginPage loginSuccess={onLoginSuccess} />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
