58ce9382339313a6a3de0a95badead044ed3c256
M.Luthfi Farid <82927435+mluthfifrd@users.noreply.github.com>
2023-11-30 07:28:08 +0700

    Add button logout

--- src/pages/layout/Header.jsx
+++ src/pages/layout/Header.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Navigation from './Navigation'
+import { FiHome, FiPlusCircle, FiLogOut } from 'react-icons/fi'
 
-function Header() {
+function Header({ logout, name, authUser }) {
+  if (authUser === null) {
+    return (
+      <header>
+        <h1>
+          <Link to="/">Aplikasi Catatan</Link>
+        </h1>
+      </header>
+    )
+  }
+
   return (
     <header>
       <h1>
         <Link to="/">Aplikasi Catatan</Link>
       </h1>
       <Navigation />
+      <button onClick={logout} className="button-logout">
+        <FiLogOut /> {name}
+      </button>
     </header>
   )
 }
